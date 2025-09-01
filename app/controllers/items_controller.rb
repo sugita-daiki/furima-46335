c3801ef87caec2ddeb3cee6bec0cece9aa931d4d
sugita-daiki <footballista.2016@gmail.com>
2025-09-01 17:58:51 +0900

    修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -2,7 +2,6 @@ class ItemsController < ApplicationController
   before_action :authenticate_user!, only: [:new, :create]
 
   def index
-    @items = @items = Item.all.order(created_at: :desc)
   end
 
   def new
