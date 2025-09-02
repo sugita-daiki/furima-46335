33de48179e55c72226113c1d78f13466473408d8
sugita-daiki <footballista.2016@gmail.com>
2025-09-02 17:46:21 +0900

    ログインの有無

--- config/routes.rb
+++ config/routes.rb
@@ -4,5 +4,5 @@ Rails.application.routes.draw do
 
   root "items#index"
   resources :users, only: [:edit, :update]
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create, :show]
 end
