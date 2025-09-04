c6ae17fa0718e1544ed6f62653dc06afc7c00f62
sugita-daiki <footballista.2016@gmail.com>
2025-09-04 21:36:30 +0900

    orderコントローラーの編集

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,7 +1,22 @@
 class OrdersController < ApplicationController
   def index
+    @order_address = OrderAddress.new
   end
 
   def create
+    @order_address = OrderAddress.new(order_address_params.merge(user_id: current_user.id, item_id: @item.id))
+    if @order_address.valid?
+
+      @order_address.save
+      redirect_to root_path
+    else
+      render :index, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def order_address_params
+    params.require(:order_address).permit(:postal_code, :prefecture_id, :city, :block, :building, :phone_number, :token)
   end
 end
