2d0ab5bd7ba631acb7a960d0df40e4a33c0a85c3
sugita-daiki <footballista.2016@gmail.com>
2025-09-05 12:12:16 +0900

    フォームの情報をサーバーサーバーサイドに送信

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -8,15 +8,21 @@ const pay = () => {
   numberElement.mount('#number-form');
   expiryElement.mount('#expiry-form');
   cvcElement.mount('#cvc-form');
-  
+
   const form = document.getElementById('charge-form')
   form.addEventListener("submit", (e) => {
     payjp.createToken(numberElement).then(function (response) {
       if (response.error) {
       } else {
         const token = response.id;
-        console.log(token)
+        const renderDom = document.getElementById("charge-form");
+        const tokenObj = `<input value=${token} name='token' type="hidden">`;
+        renderDom.insertAdjacentHTML("beforeend", tokenObj);
       }
+      numberElement.clear();
+      expiryElement.clear();
+      cvcElement.clear();
+      document.getElementById("charge-form").submit();
     });
     e.preventDefault();
   });
